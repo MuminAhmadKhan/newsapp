a439b0fd48f6237ebabfd74b41ed399372cdbba8
MaK <muminkhan2000@gmail.com>
2022-01-05 22:08:07 +0530

    added to NewsItems

--- src/Components/News.js
+++ src/Components/News.js
@@ -9,13 +9,22 @@ export default class News extends Component {
 
     render() {
         return (
-            <div>
-               This is News
-               <NewsItem/> 
-               <NewsItem/>
-               <NewsItem/>
-               <NewsItem/>
+            <div  className="container">
+               <h3>NEWS-HEADLINES</h3>
+               <div class="row">
+               <div className="col-md-4">
+                <NewsItem/> 
+               </div> 
+               <div className="col-md-4">
+                <NewsItem/>
+                </div>
+                <div className="col-md-4">
+                <NewsItem/>  
+                </div>
+               </div>
+             
             </div>
+            
         )
     }
 }
